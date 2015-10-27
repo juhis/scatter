ec7db43067fd37050341d183fc080f33b46a6ffc
Juha Karjalainen <juha.m.karjalainen@gmail.com>
2015-10-27 18:13:57 +0100

    .gitignore fix

--- config/data.js
+++ config/data.js
@@ -1,11 +1,11 @@
 var data = {
 
-    dir: '/data/pc-eigenvectors', // directory for data files (one file per dimension)
+    dir: '/data/example', // directory for data files (one file per dimension)
     type: 'buffer', // 'buffer' or 'json'
     prefix: 'PC', // file name prefix, files go 'PC1.buffer' (or 'PC1.json'), 'PC2.buffer' etc.
     numDimensions: 100, // equal or smaller than the number of data files
     
-    annotationDir: '/data/pc-annotations', // leave empty for no annotations
+    annotationDir: '/data/example_annotations_binary', // leave empty for no annotations
     annotationType: 'buffer', // 'buffer' or 'json'
 }
 
